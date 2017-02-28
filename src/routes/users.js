68950fe1535f273da3ff7c526a3b3fbd020a28ac
Archheretic <setervang@hotmail.com>
2017-02-28 14:39:17 +0100

    added read from file in the GET /api/names route

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,9 +3,21 @@
  */
 let express = require('express');
 let router = express.Router();
+let fs = require('fs');
+let path = require('path');
+
+let namesPath = path.join(__dirname, '..', 'storage', 'names.json');
+//let jsonString = fs.readFileSync(namesPath, 'utf8');
 
 router.get('/', function(req, res) {
-    res.json({"users" : '\"name\" : \"Ole\"'});
+    let obj;
+    fs.readFile(namesPath, 'utf8', function (err, data) {
+        if (err) throw err;
+        obj = JSON.parse(data);
+        console.log(obj);
+        res.json(obj);
+    });
+
 });
 
 module.exports = router;
