d12e15c0a49ebb3574d6115b616ff9164ee63ddf
Archheretic <setervang@hotmail.com>
2017-02-28 13:22:38 +0100

    add CORS support

--- src/server.js
+++ src/server.js
@@ -16,13 +16,13 @@ app.use(bodyParser.json());
 // logs requests to the console.
 app.use(morgan("dev"));
 
-/*
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-*/
+
 app.use('/api/users', users);
 
 app.listen(3000);
